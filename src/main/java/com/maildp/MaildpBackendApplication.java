8a00fe1c2e2a6e860f60138dc7af92b2dcbca2dd
SivaDurgaPrasadKodi <sivakodi2004@gmail.com>
2026-05-31 15:10:56 +0530

    Phase 1: Spring Boot initialized - MailDP backend running on port 8080

---
+++ src/main/java/com/maildp/MaildpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.maildp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaildpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaildpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maildp/MaildpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maildp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaildpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
