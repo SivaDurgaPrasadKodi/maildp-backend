2188676d29e0029cfaf51e79821a6d0c651811ec
SivaDurgaPrasadKodi <sivakodi2004@gmail.com>
2026-06-12 19:48:39 +0530

    Fix: Add Netlify domain to CORS allowed origins

--- src/main/java/com/maildp/config/SecurityConfig.java
+++ src/main/java/com/maildp/config/SecurityConfig.java
@@ -52,9 +52,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-            "http://localhost:5173",
-            "http://localhost:5174"
-        ));
+        	    "http://localhost:5173",
+        	    "http://localhost:5174",
+        	    "https://maildp-frontend.netlify.app"
+        	));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
